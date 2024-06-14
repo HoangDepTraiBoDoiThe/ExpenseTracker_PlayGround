929e17161a261527b92b6d3d7a8b21f29097a3e9
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-06-14 20:40:15 +0700

    Add models

---
+++ Models/ApplicationDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models;
+
+public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models;
+
+public class Category
+{
+    [Key]
+    public int CategoryId { get; set; }
+    
+    [Column(TypeName = "nvarchar(50)")]
+    public int Title { get; set; }
+    
+    [Column(TypeName = "nvarchar(50)")]
+    public int Icon { get; set; }
+
+    // Expense or income
+    [Column(TypeName = "nvarchar(50)")]
+    public int Type { get; set; }
+
+}
