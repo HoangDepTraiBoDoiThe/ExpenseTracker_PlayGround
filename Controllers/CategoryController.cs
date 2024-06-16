f89b96ab3ddebfca2e3f069df44c187ffb4369aa
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-06-16 16:00:14 +0700

    Delete func
    Issue: Delete without asking

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -24,24 +24,6 @@ namespace ExpenseTracker.Controllers
             return View(await _context.Categories.ToListAsync());
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // GET: Category/Create
         public async Task<IActionResult> CreateOrEdit(int id = 0)
         {
@@ -93,24 +75,6 @@ namespace ExpenseTracker.Controllers
 
                     return RedirectToAction(nameof(Index));
                 }
-            }
-
-            return View(category);
-        }
-
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
             }
 
             return View(category);
